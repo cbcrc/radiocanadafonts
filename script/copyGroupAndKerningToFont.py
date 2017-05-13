61fa7c480ae38e42e168eed29e1d5ea50fd00db5
Alexandre Saumier Demers <asaumierdemers@gmail.com>
2017-05-13 14:23:30 -0400

    update

---
+++ script/copyGroupAndKerningToFont.py
@@ -0,0 +1,22 @@
+regularpath = "../src/Radio-Canada-Regular.ufo"
+lightpath = "../src/Radio-Canada-Light.ufo"
+boldpath = "../src/Radio-Canada-Bold.ufo"
+
+regular = OpenFont(regularpath, showUI=False)
+light = OpenFont(lightpath, showUI=False)
+bold = OpenFont(boldpath, showUI=False)
+
+groups = regular.groups
+kerning = regular.kerning
+
+light.groups.clear()
+light.kerning.clear()
+light.groups.update(groups)
+light.kerning.update(kerning)
+light.save()
+
+bold.groups.clear()
+bold.kerning.clear()
+bold.groups.update(groups)
+bold.kerning.update(kerning)
+bold.save()
---
+++ script/fixTabWidth.py
@@ -0,0 +1,17 @@
+tablist = ["asciicircum", "asciitilde", "bulletoperator", "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "plus", "minus", "plusminus", "equal", "notequal", "approxequal", "multiply", "less", "greater", "lessequal", "greaterequal", "divide", "logicalnot", "section", "Euro", "currency", "dollar", "cent", "florin", "sterling", "yen", "period.tab", "comma.tab", "colon.tab", "semicolon.tab"]
+
+tabwidth = 640
+
+font = CurrentFont()
+
+for glyphname in tablist:
+    
+    glyph = font[glyphname]
+    width = int(round(glyph.width))
+    
+    if width !=tabwidth:
+        d = tabwidth-width
+        glyph.move((d/2, 0))
+        glyph.width = tabwidth
+        glyph.update()
+        
