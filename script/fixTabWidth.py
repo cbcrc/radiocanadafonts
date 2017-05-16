cb54b641f5c889b6dcdf1b9e0d9136d43dd351ed
Alexandre Saumier Demers <asaumierdemers@gmail.com>
2017-05-16 16:09:25 -0400

    set tab width to 600

--- script/fixTabWidth.py
+++ script/fixTabWidth.py
@@ -1,6 +1,6 @@
-tablist = ["asciicircum", "asciitilde", "bulletoperator", "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "plus", "minus", "plusminus", "equal", "notequal", "approxequal", "multiply", "less", "greater", "lessequal", "greaterequal", "divide", "logicalnot", "section", "Euro", "currency", "dollar", "cent", "florin", "sterling", "yen", "period.tab", "comma.tab", "colon.tab", "semicolon.tab"]
+tablist = ["asciicircum", "asciitilde", "bulletoperator", "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "plus", "minus", "plusminus", "equal", "notequal", "approxequal", "multiply", "less", "greater", "lessequal", "greaterequal", "divide", "logicalnot", "Euro", "currency", "dollar", "cent", "florin", "sterling", "yen", "period.tab", "comma.tab", "colon.tab", "semicolon.tab"]
 
-tabwidth = 640
+tabwidth = 600
 
 font = CurrentFont()
 
@@ -11,9 +11,9 @@ for glyphname in tablist:
     
     if width !=tabwidth:
         print width, tabwidth, glyph
-        #d = tabwidth-width
-        #glyph.move((d/2, 0))
-        #glyph.width = tabwidth
-        #glyph.update()
+        d = tabwidth-width
+        glyph.move((d/2, 0))
+        glyph.width = tabwidth
+        glyph.update()
     glyph.mark = (1.0, 0.4, 1.0, 1.0)
         
