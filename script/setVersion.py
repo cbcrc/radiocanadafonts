057a9974826f0b8acfea178855a92c49b515e44c
Alexandre Saumier Demers <asaumierdemers@gmail.com>
2017-08-12 15:54:01 -0400

    bump version 1.2

--- script/setVersion.py
+++ script/setVersion.py
@@ -1,7 +1,7 @@
 for font in AllFonts():
     
     font.info.versionMajor = 1
-    font.info.versionMinor = 0
+    font.info.versionMinor = 2
     
     print "%s.%s" % (font.info.versionMajor, font.info.versionMinor)
     
