833a2bbe820d6cb090bf510bf5971322708e5c77
Alexandre Saumier Demers <asaumierdemers@gmail.com>
2017-09-07 10:38:13 -0400

    fix 'a'.alt' in light

--- script/setVersion.py
+++ script/setVersion.py
@@ -1,7 +1,7 @@
 for font in AllFonts():
     
     font.info.versionMajor = 1
-    font.info.versionMinor = 200
+    font.info.versionMinor = 201
     
     print "%s.%s" % (font.info.versionMajor, font.info.versionMinor)
     
