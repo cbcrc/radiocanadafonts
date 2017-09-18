af30920ccde09a1aefcd7469d43b7900f540432f
Alexandre Saumier Demers <asaumierdemers@gmail.com>
2017-09-18 17:57:24 -0400

    remove tab punctuation, version 1.210

--- script/setVersion.py
+++ script/setVersion.py
@@ -1,7 +1,7 @@
 for font in AllFonts():
     
     font.info.versionMajor = 1
-    font.info.versionMinor = 201
+    font.info.versionMinor = 210
     
     print "%s.%s" % (font.info.versionMajor, font.info.versionMinor)
     
