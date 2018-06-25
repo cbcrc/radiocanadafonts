975df3fceb10ee1fa198befea405314d46e9b1d8
Alexandre Saumier Demers <asaumierdemers@gmail.com>
2018-06-25 14:23:03 -0400

    fix bug with CFF FontName by updating fontmake

--- build.py
+++ build.py
@@ -3,14 +3,6 @@ import os
 
 SRC_DIR = "src"
 
-# FIXME temp removal of mark and mkmk features with an empty class
-class NoMarkFeatureWriter(object):
-	def __init__(self, font):
-		pass
-		
-	def write(self, doMark, doMkmk):
-		return None
-
 romans = [
 	os.path.join(SRC_DIR, "Radio-Canada-Light.ufo"),
 	os.path.join(SRC_DIR, "Radio-Canada-Regular.ufo"),
@@ -30,13 +22,4 @@ italics = [
 
 for ufos in [romans, italics]:
 	project = FontProject()
-
-	project.run_from_ufos(ufos, 
-		output=("otf", "ttf", "ttf-interpolatable"), 
-		designspace_path=None, 
-		remove_overlaps=True, 
-		reverse_direction=True, # only used for ttf and ttf-interpolatable
-		conversion_error=None,
-		use_production_names=False, # kwargs # FIXME which is best?
-		)#mark_writer_class=NoMarkFeatureWriter) # kwargs
-
+	project.run_from_ufos(ufos, output=("otf", "ttf", "ttf-interpolatable"), use_production_names=False, feature_writers=[])
