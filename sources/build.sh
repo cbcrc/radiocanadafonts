afc277656413bbfe7de29da249942f70c6becf7f
Eli Heuer <elih@protonmail.com>
2021-02-10 10:25:10 -0800

    Update the build script for basic VF builds (#11)

--- sources/build.sh
+++ sources/build.sh
@@ -1,3 +1,9 @@
 # !/bin/bash
 
 set -e
+
+echo "\n**** Generating VFs ****\n"
+mkdir -p fonts
+fontmake -m sources/Radio-Canada.designspace -o variable --no-production-names --output-path fonts/RadioCanada[wdth,wght].ttf
+echo "\n**** Post Processing VFs ****\n"
+echo "\n**** Done ****\n"
