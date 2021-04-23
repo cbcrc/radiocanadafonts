1f30990187cc57b6d49d8adc334f57f7b744f9ba
Eli Heuer <elih@protonmail.com>
2021-04-22 23:36:41 -0700

    Start updating sources based on proofs from Viviana

--- sources/build-test-roman.sh
+++ sources/build-test-roman.sh
@@ -6,7 +6,7 @@ fontmake \
     -m sources/Radio-Canada.designspace \
     -o variable \
     --no-production-names \
-    --output-path fonts/variable/Radio-Canada[wdth,wght].ttf
+    --output-path fonts/variable/Radio-Canada-Test-Build.ttf
 #    --verbose DEBUG 
 
 echo "\n**** Done ****\n"
